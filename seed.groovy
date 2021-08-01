ceb4714c351e08c83276c6fa77ac252b8bc50cdd
Sravanizelar <82637289+Sravanizelar@users.noreply.github.com>
2021-08-01 20:56:26 +0530

    Update seed.groovy

--- seed.groovy
+++ seed.groovy
@@ -38,23 +38,4 @@ for (i in 0..count) {
         }
     }
 }
-pipelineJob("Deployment Pipeline") {
-    configure { flowdefinition ->
-        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-                'userRemoteConfigs' {
-                    'hudson.plugins.git.UserRemoteConfig' {
-                        'url'('https://github.com/Sravanizelar/jenkins2.git')
-                    }
-                }
-                'branches' {
-                    'hudson.plugins.git.BranchSpec' {
-                        'name'('main')
-                    }
-                }
-            }
-            'scriptPath'('jenkinsfile-deployment')
-            'lightweight'(true)
-        }
-    }
-}
+
