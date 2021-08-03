da89defc2dd5ceeee29ef80302dd630f064e1919
Sravanizelar <82637289+Sravanizelar@users.noreply.github.com>
2021-08-03 20:00:36 +0530

    Update seed.groovy

--- seed.groovy
+++ seed.groovy
@@ -3,7 +3,7 @@ folder('CI-Pipelines') {
     description('CI Pipelines')
 }
 
-def component = ["frontend", "login","todo","users"];
+def component = ["todo", "login","users","frontend"];
 
 def count=(component.size()-1)
 for (i in 0..count) {
@@ -22,19 +22,39 @@ for (i in 0..count) {
                 'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
                     'userRemoteConfigs' {
                         'hudson.plugins.git.UserRemoteConfig' {
-                            'url'('https://github.com/Srihari-Zelar/'+j+'.git')
+                            'url'('https://github.com/zssravani/jenkins2'+j+'.git')
                             'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
                         }
                     }
                     'branches' {
                         'hudson.plugins.git.BranchSpec' {
-                            'name'('*/tags/*')
+                            'name'('*/main')
                         }
                     }
                 }
-                'scriptPath'('Jenkinsfile')                
+                'scriptPath'('jenkinsfile')
                 'lightweight'(true)
             }
+        }
+    }
+}
+pipelineJob("Deployment Pipeline") {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/zssravani/jenkins2.git')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('main')
+                    }
+                }
+            }
+            'scriptPath'('jenkinsfile-deployment')
+            'lightweight'(true)
         }
     }
 }
